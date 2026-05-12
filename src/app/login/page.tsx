da4343ee0df298960c6487d6006d95fec7350870
nokami <qyeshang5@gmail.com>
2026-05-13 04:47:11 +0900

    importの修正

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Suspense, useEffect, useState } from "react";
+import { Suspense, useState } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
 
 import BackButton from "@/components/BackButton";
